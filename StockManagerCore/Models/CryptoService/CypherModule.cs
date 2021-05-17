e899a67e498026da1c5f87f4143b5d5b7814753c
André Moretto <amoretto@outlook.com.br>
2021-05-17 17:22:02 -0300

    Created user structure with encriptation methods for password

---
+++ StockManagerCore/Models/CryptoService/CypherModule.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace StockManagerCore.Models.CryptoService
+{
+	/// <summary>
+	/// Module for encript the system password and put them on database.
+	/// Public methods for encriptation and comparisson.
+	/// Private methods those who create hash and eventualy compares them
+	/// </summary>
+	public static class CypherModule
+	{
+		public static string MD5Return( string password ) //Public Metod to return encripted value
+		{
+			try
+			{
+				using ( MD5 md5Hash = MD5.Create() )
+				{
+					return HashReturn( md5Hash, password );
+				}
+			}
+			catch ( CryptographicException ex )
+			{
+				string message = ex.Message;
+				if ( ex.InnerException != null )
+				{
+					message += "\n" + ex.InnerException;
+				}
+				throw new CryptographicException( message );
+			}
+		}
+
+		public static bool MD5Compare( string dbPass, string MD5Pass ) //Comparrison result return method
+		{
+			try
+			{
+				using ( MD5 md5Hash = MD5.Create() )
+				{
+					var Password = MD5Return( dbPass );
+					if ( HashCheck( md5Hash, MD5Pass, Password ) )
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			catch ( CryptographicException ex )
+			{
+				string message = ex.Message;
+				if ( ex.InnerException != null )
+				{
+					message += "\n" + ex.InnerException;
+				}
+				throw new CryptographicException( message );
+			}
+		}
+
+		private static string HashReturn( MD5 md5Hash, string input ) //Private encriptor method 
+		{
+			StringBuilder sb = new StringBuilder();
+			try
+			{
+				byte[] inputData = md5Hash.ComputeHash( Encoding.UTF8.GetBytes( input ) );
+
+				for ( int i = 0; i < inputData.Length; i++ )
+				{
+					sb.Append( inputData[i].ToString( "x2" ) );
+				}
+			}
+			catch ( CryptographicException ex )
+			{
+				string message = ex.Message;
+				if ( ex.InnerException != null )
+				{
+					message += "\n" + ex.InnerException;
+				}
+				throw new CryptographicException( message );
+			}
+			return sb.ToString();
+		}
+
+		private static bool HashCheck( MD5 md5Hash, string input, string hash ) //Comparrison method
+		{
+			try
+			{
+				StringComparer scComparisson = StringComparer.OrdinalIgnoreCase;
+
+				if ( 0 == scComparisson.Compare( input, hash ) )
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			catch ( CryptographicException ex )
+			{
+				string message = ex.Message;
+				if ( ex.InnerException != null )
+				{
+					message += "\n" + ex.InnerException;
+				}
+				throw new CryptographicException( message );
+			}
+		}
+	}
+}
--- StockManagerCore/Models/User.cs
+++ StockManagerCore/Models/User.cs
@@ -1,48 +1,98 @@
-﻿using StockManagerCore.Models.Enum;
+﻿#region --== Using includes ==--
+using StockManagerCore.Models.Enum;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
+using StockManagerCore.Models.CryptoService;
+using System.Windows;
+using System;
+#endregion
 
 namespace StockManagerCore.Models
 {
 	public class User
 	{
 
+		#region --== Properties ==--
 		[Key]
 		public int Id { get; set; }
 		public string UserName { get; set; }
 		public UserType UserType { get; set; }
 		public string Email { get; set; }
-		public string Password { get; private set; }
+		private string MD5Password { get; set; }
+		#endregion
+
+		#region --== Private Props ==--
 		[NotMapped]
 		private string Keychange;
+		#endregion
 
-		public User()
-		{
-		}
+		#region --== Constructors ==--
+		public User() { }
 
-		public User( string userName )
+		public User( string userName, UserType userType, string email, string openPass )
 		{
 			UserName = userName;
+			UserType = userType;
+			Email = email;
 			Keychange = "";
+			SetPass( openPass );
 		}
+		#endregion
 
+		#region --== Public Methods ==--
 		public bool SetPass( string password )
 		{
-			Keychange = password;
-			Password = CryptoPass(Keychange);
-
-			return true;
+			try
+			{
+				Keychange = password;
+				MD5Password = CypherPass( Keychange );
+				return true;
+			}
+			catch ( ApplicationException ex  )
+			{
+				MessageBox.Show( 
+						ex.Message, 
+						"Erro ao gravar senha!", 
+						MessageBoxButton.OK, 
+						MessageBoxImage.Error );
+			}
+			return false;
 		}
-		private string CryptoPass( string pass )
+		public bool CheckPass( string passKey )
 		{
-			StringBuilder sb = new StringBuilder();
-			return sb.ToString();
+			if ( CypherCheck( passKey ) )
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
-		private string DeCryptPass( string pass )
+		public bool ChangePass(string passkey, string newPass)
 		{
-			StringBuilder sb = new StringBuilder();
-			return sb.ToString();
+			if ( CheckPass( passkey ) )
+			{
+				return SetPass( newPass );
+			}
+			else
+			{
+				MessageBox.Show(
+						"Não foi possivel trocar a senha!\n Verifique a senha atual e tente novamente!",
+						"Erro ao gravar senha!",
+						MessageBoxButton.OK,
+						MessageBoxImage.Error );
+				return false;
+			}
+						
 		}
+		#endregion
+	
+		#region --== Private Methods ==--
+		private string CypherPass( string password )
+		{ return CypherModule.MD5Return( password ); }
+		private bool CypherCheck( string passKey )
+		{ return CypherModule.MD5Compare( passKey, MD5Password ); }
+		#endregion
 	}
 }
